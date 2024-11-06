050125e38bf5c783b9ff926e92851716b49fcd45
“marcosnchez18” <marcoantonio.sanchez.monge@iesdonana.org>
2024-11-06 11:09:55 +0100

    solucionadoo

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import requests
 
-# URL de la API
+
 url = "http://10.58.6.89:8082/cliente/103728/products"
 
 try:
-    # Realiza la solicitud GET a la API
+  
     response = requests.get(url)
     
-    # Verifica si la solicitud fue exitosa
+   
     if response.status_code == 200:
-        # Procesa la respuesta JSON
+      
         data = response.json()
         print("Datos recibidos de la API:")
         print(data)
--- funcional.py
+++ funcional.py
@@ -1,22 +1,22 @@
 import requests
 
-# URL de la API
+
 url = "http://10.58.6.89:8082/get_data_client_baja_for_dni"
 
-# DNI que quieres buscar
-dni = "52322185Z"  # Reemplaza esto con el DNI que deseas buscar
 
-# Datos a enviar en el cuerpo de la petición POST
+dni = "52322185Z"  
+
+
 data = {
     "dni": dni
 }
 
-# Haciendo la petición POST a la API
+
 response = requests.post(url, json=data)
 
-# Comprobando si la petición fue exitosa
+
 if response.status_code == 200:
-    # Procesar la respuesta JSON
+    
     datos_cliente = response.json()
     print(datos_cliente)
 else:
